b983d8713f7956b4a4c9a74a2d730fa24f51e799
sf1024z <sf1024z@sf1024zui-MacBook-Pro.local>
2021-04-10 16:09:14 +0900

    폴더 변경

--- src/typescript/Test/02_Class/02_InterFaceTest.ts
+++ src/typescript/Test/02_Class/02_InterFaceTest.ts
@@ -6,8 +6,10 @@ interface InsertMem{
 var insert = function(mem:InsertMem){
     console.log(mem.a+","+mem.b);
 }
-var newMem:InsertMem;
-var a:String;
-newMem.a = "b";
+var newMem:InsertMem = {
+    a:"b"
+    ,b:2//b를 생성하지 않으면 에러가 발생
+}
+newMem.a = "C";
 insert(newMem);
 console.log("aaaa");
